74ac40d62251a7f7265452100d7b1a116c67d1ad
ana-munoz <33760974+ana-munoz@users.noreply.github.com>
2026-08-26 11:35:24 -0400

    Feat/hu 05 subnavbar (#33)
    * feat: refactorización de header y footer footer components + navegación a comunidad HU-02: navegación entre vistas #18
    
    * feat: implementación de subnavbar + navegación a cambios y devoluciones
    HU-02: navegación entre vistas
     #18

--- componentes.js
+++ componentes.js
@@ -4,5 +4,30 @@ async function cargarHeaderFooter(url, contenedorId) {
     document.getElementById(contenedorId).innerHTML = contenido;
 }
 
-cargarHeaderFooter('header.html', 'header-component');
-cargarHeaderFooter('footer.html', 'footer-component');
+async function init() {
+    await cargarHeaderFooter('header.html', 'header-component');
+    await cargarHeaderFooter('footer.html', 'footer-component');
+
+    // Recién aquí el header ya existe en el DOM
+    const navAyuda = document.querySelector('.nav-ayuda');
+    const navDropdown = document.querySelector('.nav-dropdown');
+    const subnavbar = document.querySelector('.subnavbar');
+
+
+// evento para mostrar/ocultar el subnavbar al hacer click en Ayuda
+navAyuda.addEventListener('click', (evento) => {
+    evento.preventDefault();
+    subnavbar.classList.toggle('activo');
+});
+
+//al pasar el mouse por encima de Ayuda, se muestra el subnavbar
+navDropdown.addEventListener('mouseenter', () => {
+    subnavbar.classList.add('activo');
+});
+
+//al quitar el mouse de Ayuda, se oculta el subnavbar
+navDropdown.addEventListener('mouseleave', () => {
+    subnavbar.classList.remove('activo');
+});
+}
+init();
